e23effb1d70bfadbc3744a1d844f9c19edbb70a1
Omer Haim Israeli <omeri@elementor.com>
2025-05-18 15:55:39 +0300

    wip

--- actions/update-versions-in-elementor/tsup.config.ts
+++ actions/update-versions-in-elementor/tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['./index.ts'],
-	format: ['cjs'],
+	entry: ['index.ts'],
+	outDir: 'dist',
+	format: 'cjs',
+	noExternal: [/.+/],
+	platform: 'node',
+	minify: true,
 	clean: true,
-	noExternal: [
-		// Include all dependencies in the bundle
-		'.*',
-	],
-}); 
+});

45d67a7eb003fd353c7c1bec4fccf16253e8dde7
Omer Haim Israeli <omeri@elementor.com>
2025-05-19 13:34:30 +0300

    wip

--- actions/update-versions-in-elementor/main.ts
+++ actions/update-versions-in-elementor/main.ts
@@ -57,7 +57,7 @@ export async function run() {
 	        await exec.exec(`git config user.email ${internalBotEmail}`);
             
             // Set the token for authentication
-            const repoUrl = `https://x-access-token:${token}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`;
+            const repoUrl = `https://omerisra6:${token}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`;
             await exec.exec('git', ['remote', 'set-url', 'origin', repoUrl]);
         });
 

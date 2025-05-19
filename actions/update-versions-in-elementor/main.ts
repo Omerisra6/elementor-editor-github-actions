c93957222ffb3f842531131e3ea688e5252498ab
Omer Haim Israeli <omeri@elementor.com>
2025-05-19 13:17:15 +0300

    wip

--- actions/update-versions-in-elementor/main.ts
+++ actions/update-versions-in-elementor/main.ts
@@ -55,7 +55,7 @@ export async function run() {
         await core.group('Setting up git configuration', async () => {
             await exec.exec(`git config user.name "elementorbot"`);
 	        await exec.exec(`git config user.email ${internalBotEmail}`);
-            core.debug(`is token exists? ${!! token}`);
+            core.info(`is token exists? ${!! token}`);
             
             // Set the token for authentication
             const repoUrl = `https://x-access-token:${token}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`;
